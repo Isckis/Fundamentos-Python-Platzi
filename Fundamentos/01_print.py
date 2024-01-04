11d93dcc492e6ec06edd8ec41e4d44218515a237
Isaías <isckis@gmail.com>
2024-01-03 23:10:57 -0400

    Fundamentos 1 y 2

---
+++ Fundamentos/01_print.py
@@ -0,0 +1,19 @@
+print("Hola, esto es phyton")
+
+print("hola soy Isaías y estoy aprendiendo a programar pyton")
+
+#Haremos un comentario con #
+
+print (13+5)
+
+print (10 - 5)
+
+print (2 * 3)
+
+print (16 / 8)
+
+#Cuidado con los IndentationError al poner espacio en al comienzo de las línas de código.
+
+"""
+esto es un comentario que podría tener más de una línea
+"""
---
+++ Fundamentos/02_vars.py
@@ -0,0 +1,18 @@
+print("Hola, vars")
+#esto es una variables de caractéres
+my_name = "Isaías"
+
+print (my_name)
+#esto es una variable con número
+my_age = 6
+
+print (my_age)
+
+my_name = "Ignacio"
+
+print ("aquí cambió", my_name)
+
+#input
+
+my_name = input ("cuál es tu nombre?")
+print("usando input", my_name)
